3ba89dde915104da0bc1a5aa2e1f780f2850a4e3
NopphadolKa <NopphadolKa@gmail.com>
2025-10-31 16:15:40 +0700

    Leaflet

--- client/src/components/map/MapView.jsx
+++ client/src/components/map/MapView.jsx
@@ -1,6 +1,21 @@
+import { MapContainer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import Layers from "./Layers";
+
 const MapView = () => {
+  const center = [13, 100];
+
   return (
-    <div className="flex-1">MapView</div>
-  )
-}
-export default MapView
+    <div className="flex-1">
+      <MapContainer
+        className="h-full"
+        center={center}
+        zoom={7}
+        scrollWheelZoom={true}
+      >
+        <Layers />
+      </MapContainer>
+    </div>
+  );
+};
+export default MapView;
